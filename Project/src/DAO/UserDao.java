9384126c6244dd4fd2924d57ce65371e47c8863e
ToshikiTomoyose <sf7fantazy7assaltsk@gmail.com>
2017-10-30 17:07:29 +0900

    sqlnikakikomitai

--- Project/src/DAO/UserDao.java
+++ Project/src/DAO/UserDao.java
@@ -4,30 +4,37 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+//import dao.DBManager;
+//import model.Employee_bean;
 import model.Userbean;
 
 public class UserDao {
-	public Userbean findBy(id) {
+	public List<Userbean> findAll() {
         Connection conn = null;
-        Userbean empList = new Userbean();
+        List<Userbean> uselist = new ArrayList<Userbean>();
 
         try {
             // データベースへ接続
             conn = DBManager.getConnection();
 
             // SELECT文を準備
-            String sql = "SELECT id, name, age FROM UserManagement";
+            String sql = "SELECT id, pass FROM User";
 
              // SELECTを実行し、結果表を取得
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
 
+            // 結果表に格納されたレコードの内容を
+            // Employeeインスタンスに設定し、ArrayListインスタンスに追加
             while (rs.next()) {
                 String id = rs.getString("id");
                 String pass = rs.getString("pass");
-                Userbean usebean = new Userbean(id, pass);
-                return usebean;
+//                int age = rs.getInt("age");
+                Userbean employee = new Userbean(id, pass);
+                uselist.add(employee);
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -43,6 +50,6 @@ public class UserDao {
                 }
             }
         }
-        ;
+        return uselist;
     }
 }
